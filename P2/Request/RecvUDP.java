7456be87da353f8404be7f2911ebc5af5e71d33b
Dylan <dcm0033@auburn.edu>
2019-10-01 14:35:19 -0500

    Server work

--- P2/Request/RecvUDP.java
+++ P2/Request/RecvUDP.java
@@ -21,8 +21,32 @@ public class RecvUDP {
 				  new RequestDecoderBin(args[1]) :
 				  new RequestDecoderBin() );
 
-
+	int answer;
       Request receivedRequest = decoder.decode(packet);
+	  if (recievedRequest.op_code == 1){
+		answer = recievedRequest.operand_1 + receivedRequest.operand_2;
+	  }
+	  if (recievedRequest.op_code == 2){
+		answer = recievedRequest.operand_1 - receivedRequest.operand_2;
+	  }
+	  if (recievedRequest.op_code == 3){
+		answer = recievedRequest.operand_1 * receivedRequest.operand_2;
+	  }
+	  if (recievedRequest.op_code == 4){
+		answer = recievedRequest.operand_1 / receivedRequest.operand_2;
+	  }
+	  if (recievedRequest.op_code == 5){
+		answer = recievedRequest.operand_1 >> receivedRequest.operand_2;
+	  }
+	  if (recievedRequest.op_code == 6){
+		answer = recievedRequest.operand_1 << receivedRequest.operand_2;
+	  }
+	  if (recievedRequest.op_code == 7){
+		answer = ~recievedRequest.operand_1;
+	  }
+	  //create exit packet
+	  int tml = 6;
+	  int request_id = recievedrequest.request_id;
 
       System.out.println("Received Binary-Encoded Request");
       System.out.println(receivedRequest);
