eba7cb848dd274288fd46d01048d7bd78ed7db18
Ian Brown <icb0004@auburn.edu>
2019-10-22 16:41:57 -0500

    send tcp

--- P3/Request/SendTCP.java
+++ P3/Request/SendTCP.java
@@ -1,32 +1,97 @@
-import java.io.*;   // for Input/OutputStream
-import java.net.*;  // for Socket
+import java.io.*; // for Input/OutputStream
+import java.net.*; // for Socket
+import java.util.*; 
 
+// client
 public class SendTCP {
+    public static void main(String args[]) throws Exception {
 
-  public static void main(String args[]) throws Exception {
+        if (args.length != 2) // Test for correct # of args
+            throw new IllegalArgumentException("Parameter(s): <Destination> <Port>");
 
-    if (args.length != 2)  // Test for correct # of args
-      throw new IllegalArgumentException("Parameter(s): <Destination> <Port>");
+        InetAddress destAddr = InetAddress.getByName(args[0]); // Destination address
+        int destPort = Integer.parseInt(args[1]); // Destination port
 
-    InetAddress destAddr = InetAddress.getByName(args[0]);  // Destination address
-    int destPort = Integer.parseInt(args[1]);               // Destination port
+        Socket sock = new Socket(destAddr, destPort);
 
-    Socket sock = new Socket(destAddr, destPort);
+        Scanner sin = new Scanner(System.in);
+        RequestEncoder encoder = (args.length == 2 ? new RequestEncoderBin(args[1]) : new RequestEncoderBin());
+        int quit = 0; 
+        byte count = 1;
 
-    Friend friend = new Friend(1234567890987654L, "John Smith", 
-			       (short) 2360, 36830, false, true, false);
-    
-    System.out.println("Display friend"); 
-    System.out.println(friend); // Display friend just to check what we send
+        long start = System.currentTimeMillis();
+        
+
+       
+
+        // Business logic
+        while (quit == 0) {
+            byte op_in;
+            System.out.println("Enter the operation you'd like to perform\n 1: addition" + "\n 2: subtraction"
+                    + "\n 3: multiplication" + "\n 4: division" + "\n 5: shift right" + "\n 6: shift left"
+                    + "\n 7: one's complement");
+            op_in = sin.nextByte();
+            short[] operands = new short[255];
+            byte tml = 8;
+            byte num_operands = 0; 
+            Request req = new Request(tml, count, op_in, num_operands, operands);
+
+            if (op_in < 7) {
+                short oper_1;
+                short oper_2;
+                System.out.print("Enter first operand: ");
+                oper_1 = sin.nextShort();
+                System.out.print("\nEnter 2nd operand (shift distance): ");
+                oper_2 = sin.nextShort();
+                operands[0] = oper_1;
+                operands[1] = oper_2;
+                tml = 8; 
+                num_operands = 2;
+                req = new Request(tml, count++, op_in, num_operands, operands);
+            }
+            else if (op_in == 7) {
+                short oper_1;
+                System.out.print("Enter operand: ");
+                oper_1 = sin.nextShort();
+                operands[0] = oper_1;
+                tml = 6; 
+                num_operands = 1; 
+                req = new Request(tml, count++, op_in, num_operands, operands);
+            } else {
+                System.out.println("INVALID INPUT");
+                continue;
+            }
+
+            // encode response
+            try {
+                byte[] codedRequest = encoder.encode(req); // Encode friend
+                // debugging purposes
+                System.out.println(Arrays.toString(codedRequest));
+
+                OutputStream out = sock.getOutputStream();
+                out.write(encoder.encode(req));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
 
-    FriendEncoder encoder = new FriendEncoderBin();
+            DataInputStream in = new DataInputStream(sock.getInputStream()); 
+            long end = System.currentTimeMillis();
+            byte new_tml = in.readByte();
+            byte request_id = in.readByte();
+            byte err = in.readByte(); 
+            int answer = in.readInt();
 
-    System.out.println("Sending Friend (Binary)");
-    OutputStream out = sock.getOutputStream(); // Get a handle onto Output Stream
-    out.write(encoder.encode(friend)); // Encode and send
+            System.out.println("Request ID: " + request_id);
+            System.out.println("Answer: " + answer);
 
-    sock.close();
+            System.out.println("The request took " + Long.toString(end - start) + " milliseconds.");
 
-  }
+            System.out.println("Press 0 to continue and 1 to quit: ");
+            if (sin.nextInt() == 1) {
+                quit = 1;
+            } 
+        }
+        sock.close();
+    }
 }
